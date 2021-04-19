17ff41a0e850c1e36f8c140ae89123b3d58d453b
Sergio Brener <sergio.brener95@gmail.com>
2021-04-19 17:09:30 -0300

    :tada: project complete

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8e8f4c43-1dcd-48e8-ac39-8c91f21390b0';
+export const MODAL_CLOSE_SEC = 2.5;
